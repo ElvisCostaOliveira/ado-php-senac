23ddc2f9a74fddcedcdcba68de80ddb0570c400d
Elvis Oliveira <112600943+ElvisCostaOliveira@users.noreply.github.com>
2023-04-13 20:45:55 -0300

    Update nomes.js

--- nomes.js
+++ nomes.js
@@ -1,3 +1,3 @@
 function nomesDoAdo() {
-    return [ "Elvis Costa de Oliveira" ];
+    return [ "Elvis Costa de Oliveira","Carlos Alberto Hardman Junior","Anthony Stapf","Claudio de Souza Junior"];
 }
